05af178bae2ba13994813172ec9d9adf388cd4a6
harshul786 <harshul.n786@gmail.com>
2023-10-31 11:29:59 +0530

    Feat: remove from favt

--- src/Pages/FavoritesPage/index.tsx
+++ src/Pages/FavoritesPage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const FavoritesPage: React.FC = () => {
   type Recipe = {
@@ -31,9 +31,34 @@ const FavoritesPage: React.FC = () => {
     }[];
     instructions: string[];
   };
-  const storedFavorites = localStorage.getItem("favorites");
 
-  const favorites = storedFavorites ? JSON.parse(storedFavorites) : [];
+  const [storedFavorites, setFavorites] = React.useState<String>(
+    JSON.parse(localStorage.getItem("favorites") || "")
+  );
+  const [selectedRecipe, setSelectedRecipe] = React.useState<Recipe | null>(
+    null
+  );
+
+  const handleCloseModal = () => {
+    setSelectedRecipe(null);
+  };
+
+  const [favorites, setCurrFavorites] = useState<Recipe[]>([]);
+  useEffect(() => {
+    const storedFavorites = localStorage.getItem("favorites");
+    if (storedFavorites) {
+      setCurrFavorites(JSON.parse(storedFavorites));
+    }
+  }, []);
+
+  const handleRemoveFavorites = (recipe: Recipe) => {
+    const updatedFavorites = favorites.filter(
+      (favRecipe: any) => favRecipe.uri !== recipe.uri
+    );
+    setFavorites(JSON.stringify(updatedFavorites));
+    setCurrFavorites(updatedFavorites);
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+  };
 
   return (
     <div className="bg-gradient-to-r from-blue-500 to-purple-500 min-h-screen flex items-center justify-center">
@@ -57,6 +82,20 @@ const FavoritesPage: React.FC = () => {
                   </h3>
                 </div>
                 {/* Add a button to remove from favorites */}
+                <div className="flex justify-between tracking-wider text-black">
+                  <button
+                    onClick={() => setSelectedRecipe(recipe)}
+                    className="hover:font-medium duration-300"
+                  >
+                    View
+                  </button>
+                  <button
+                    onClick={() => handleRemoveFavorites(recipe)}
+                    className="hover:font-medium duration-200"
+                  >
+                    Remove
+                  </button>
+                </div>
               </div>
             ))
           ) : (
@@ -64,6 +103,59 @@ const FavoritesPage: React.FC = () => {
           )}
         </div>
       </div>
+      {selectedRecipe && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="flex items-center justify-center h-[90vh] overflow-y-scroll">
+            <div className="modal-overlay absolute inset-0 bg-gray-600 opacity-50"></div>
+            <div className="modal-container h-[90vh] bg-white w-11/12 md-max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+              <div className="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+                <button onClick={handleCloseModal}>Close</button>
+              </div>
+              <div className="modal-content h-[90vh] py-4 text-left px-6 pt-10">
+                <img
+                  src={selectedRecipe.image}
+                  alt={selectedRecipe.label}
+                  className="object-cover w-64 h-48 rounded-lg mx-auto"
+                />
+                <h3 className="text-xl text-clip font-semibold text-center mt-4">
+                  {selectedRecipe.label}
+                </h3>
+                <p>Source: {selectedRecipe.source}</p>
+                <h4 className="text-lg font-semibold">Ingredients:</h4>
+                <ul>
+                  {selectedRecipe.ingredients?.map((ingredient: any, index) => (
+                    <li key={index} className="flex gap-2 items-center">
+                      <div>
+                        {" "}
+                        {`${ingredient.text} - ${ingredient.quantity} ${
+                          ingredient.measure
+                            ? ingredient.measure === "<unit>"
+                              ? "unit"
+                              : ingredient.measure
+                            : ""
+                        }`}
+                      </div>
+                      <img
+                        src={ingredient.image}
+                        alt=""
+                        className="h-10 w-10 "
+                      />
+                    </li>
+                  ))}
+                </ul>
+                <h4 className="text-lg font-semibold">Cooking Instructions:</h4>
+                <ol>
+                  {selectedRecipe.instructions?.map(
+                    (instruction: string, index) => (
+                      <li key={index}>{instruction}</li>
+                    )
+                  )}
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
